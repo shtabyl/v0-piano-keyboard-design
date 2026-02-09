38cea780f53ded97c4be41acc3e561d3b7d5cb37
shtabyl <avnirog@gmail.com>
2026-02-09 09:31:57 +0300

    Fix: Add heading

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import CardSlots from "@/components/card-slots"
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-end bg-neutral-100 p-8 gap-8">
+      <h1 className="text-4xl font-bold text-muted-foreground text-heading mb-4 self-center tracking-widest">
+        Scale Master
+      </h1>
       <CardSlots />
       <PianoKeyboard />
     </main>
